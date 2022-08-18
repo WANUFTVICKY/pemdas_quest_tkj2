2941535709c5f8d6189fae1cc0ccecfed44122fd
WANUFTVICKY <111479840+WANUFTVICKY@users.noreply.github.com>
2022-08-18 00:01:41 -0700

    Add files via upload

---
+++ python2_wanu.py
@@ -0,0 +1,8 @@
+nama_depan="wanu"
+nama_tengah="aksa"
+nama_belakang="tama"
+nama_lengkap="wanu aksa tama"
+print("nama depan saya aalah",nama_depan)
+print("nama tengah saya adalah",nama_tengah)
+print("nama belakang saya adalah",nama_belakang)
+print("nama lengkap saya adalah",nama_depan,nama_tengah,nama_belakang)
