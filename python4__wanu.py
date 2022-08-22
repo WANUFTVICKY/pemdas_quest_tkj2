8730852fd0a3fa6fa9d9dab58ee019b3bc1ce59b
WANUFTVICKY <111479840+WANUFTVICKY@users.noreply.github.com>
2022-08-21 23:25:41 -0700

    Add files via upload

---
+++ python4__wanu.py
@@ -0,0 +1,12 @@
+panjang = int(input("masukan nilai panjang: "))
+lebar = int(input("masukan nilai lebar:"))
+luas_pp = panjang * lebar 
+
+print("luas persigi panjang adalah: ", luas_pp)
+
+print("3. segitiga")
+
+alas =int(input("msukan nilai alas: "))
+tingi =int(input("masukan nilai tingi: "))
+luas_s = alas * tingi /2
+print("luas segita adalah", luas_s)
