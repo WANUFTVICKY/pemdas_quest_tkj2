c3623da7fa449309a6feb1c22403aa2bda19d7c9
WANUFTVICKY <111479840+WANUFTVICKY@users.noreply.github.com>
2022-08-25 18:44:22 -0700

    Add files via upload

---
+++ python6-wanu.py
@@ -0,0 +1,21 @@
+alas = int (input("masukan nilai alas"))
+tingi = int (input("masukan nilai tingi"))
+luas_s = alas * tingi/2
+ 
+panjang = int (input("masukan panjang:"))
+lebar = int(input("masukan lebar:"))
+keliling = 2* (panajng + lebar)
+luas = panjang * lebar
+print("keliling:",keliling)
+print("luas:" ,luas)
+
+r = int (input("masukan jari jari:"))
+keliling = 2 * 3.14 *r
+luas = 3.14 * r * r
+print ("keliling=",keliling)
+print ("luas =",luas)
+
+alas = float(input("nilai alas: "))
+tingi = float(input("nilai tingi:"))
+luas_jg = alas * tingi
+print("kuas jajargenjang adalah" , luas_jg)
