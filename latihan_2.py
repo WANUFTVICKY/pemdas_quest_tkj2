33ec3525e5d8fc9715829159123ec4870fc6433d
WANUFTVICKY <111479840+WANUFTVICKY@users.noreply.github.com>
2023-02-13 09:36:52 +0700

    Add files via upload

---
+++ latihan_2.py
@@ -0,0 +1,7 @@
+'''
+buatlah program menghitung luas bangun daftar mengunakan fungsi. gunakan parameter
+- segitiga
+- persegi panjang
+'''
+
+def segitiga(alas,tingi)
---
+++ latihan_fungsi.py
@@ -0,0 +1,49 @@
+'''funsi pada python didefoinisikan dengan def'''
+'''def nama_fungsi():'''
+
+def salam():
+     print("assalamu'alaikum !! akhi ukhty")
+
+salam()
+salam()
+salam()
+
+'''buatlah sebuah fungsi vrsi kalian'''
+
+def menyapa(): 
+    print("halo !! kawan")
+
+menyapa()
+menyapa()
+menyapa()
+
+'''fungsi dengan parameter'''
+'''def nama_fungsi(p1, p2)'''
+
+def say_hi(nama):
+    print("halo perkenalkan nama saya", nama)
+
+say_hi("wanu")
+say_hi("dowers") 
+
+'''buatlah funsi dengan 3 parameter'''
+
+def welcome(nama,umur,kelas):
+    print("hay nama saya:", nama)
+    print("umur:",umur)
+    print("klas:",kelas)
+
+welcome("wanu","15","10")
+
+
+
+
+
+    
+
+
+
+
+
+ 
+
